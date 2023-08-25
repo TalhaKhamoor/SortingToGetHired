4ba4ebdd7d2b2f75a6bc4e0492e63c5d4a8551a5
Talha Khamoor <talhakhamoor@Talhas-MacBook-Air.local>
2023-08-24 19:00:15 -0600

    Fix variable and add extra logical check

--- src/SearchAlgos/LinearSearchString.java
+++ src/SearchAlgos/LinearSearchString.java
@@ -2,16 +2,18 @@ package SearchAlgos;
 
 public class LinearSearchString {
     public static void main(String[] args) {
-        String name = "Talha";
+        String name = "";
         char target = 'l';
         boolean isTargetFound = search(name, target);
         System.out.println("Target '" + target + "' was found in string '" + name + "' : " + isTargetFound);
     }
 
-    public static boolean search(String str, char target){
-        for(int i=0; i < str.length(); i++){
-            if(str.charAt(i) == target){
-                return true;
+    public static boolean search(String str, char target) {
+        if (!str.isEmpty()) {
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == target) {
+                    return true;
+                }
             }
         }
         return false;
