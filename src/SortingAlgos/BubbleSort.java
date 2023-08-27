6694a6acb1a8c0f2c77deef165fdf39467ddc362
Talha Khamoor <talhakhamoor@Talhas-MacBook-Air.local>
2023-08-27 12:50:32 -0600

    Add complete bubble sort

--- src/SortingAlgos/BubbleSort.java
+++ src/SortingAlgos/BubbleSort.java
@@ -1,18 +1,30 @@
 package SortingAlgos;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = { 2, 3, 5, 10, 4, 8, 6, 7, 1, 9};
         int[] sortedArray = bubbleSort(arr);
+        System.out.println(Arrays.toString(sortedArray));
     }
 
     private static int[] bubbleSort(int[] arr) {
-        for(i=0; i < arr.length; i++){
-            for(j = 1; j < arr.lenght - i; j++){
-                if(arr[i] > arr[j]){
-                    temp =
+        boolean swapped;
+        for(int i=0; i < arr.length; i++){
+            swapped = false;
+            for(int j = 1; j < arr.length - i; j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
                 }
             }
+            if(!swapped){
+                break;
+            }
         }
+        return arr;
     }
 }
